e0c176b68b9a0fc852086d674ae6f080d802e88e
heliotech <37197149+heliotech@users.noreply.github.com>
2021-01-01 23:57:48 +0100

    Update start.py
    Some example of new code.

--- start.py
+++ start.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """ Brand new source file in new repository """
+import numpy as np
 from khutils import printhead
 
 title = "start"
 
 def main():
   printhead(title, xline=True)
+  
+  resolution = 2000
+  x = np.linspace(0, 10, resolution)
+  y = x **2
   
   
 if __name__ = "__main__":
