bc8bf3d59faca67d094a2ab89f4087d312ee0128
heliotech <37197149+heliotech@users.noreply.github.com>
2021-01-01 23:50:30 +0100

    Create start.py

---
+++ start.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" Brand new source file in new repository """
+from khutils import printhead
+
+title = "start"
+
+def main():
+  printhead(title, xline=True)
+  
+  
+if __name__ = "__main__":
+  main()
